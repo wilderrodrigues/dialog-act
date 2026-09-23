90e39f42e029f15f776ff460f18949298018d01d
Wilder Rodrigues <wilder.rodrigues@harmattan.ai>
2026-09-23 20:14:01 +0200

    feat: Saves the best model based on the accuracy.

--- src/uu/msc/ai/mair/dialog/core/engine.py
+++ src/uu/msc/ai/mair/dialog/core/engine.py
@@ -1,14 +1,19 @@
+import math
+
+import time
 import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from sklearn.metrics import accuracy_score
 import logging
 
+from uu import get_root
+
 logging.basicConfig(level=logging.INFO)
 
 @torch.no_grad()
 def calculate_loss_accuracy(model: torch.nn.Module, loss_fn: torch.nn.Module,
-                            val_loader: DataLoader, device: torch.device, mode: str) -> None:
+                            val_loader: DataLoader, device: torch.device, mode: str) -> float:
     targets, predictions, losses = [],[],[]
     for utterances_input, acts_output in val_loader:
         utterances_input = utterances_input.to(device)
@@ -26,11 +31,16 @@ def calculate_loss_accuracy(model: torch.nn.Module, loss_fn: torch.nn.Module,
     logging.info(f"{mode} Loss : {torch.tensor(losses).mean():.3f}")
     accuracy = accuracy_score(targets.detach().cpu().numpy(), predictions.detach().cpu().numpy())
     logging.info(f"{mode} Accuracy  : {accuracy:.3f}")
+    return accuracy
 
 
 def train_loop(model: torch.nn.Module, loss_fn: torch.nn.Module, optimizer: torch.optim.Optimizer,
                train_loader: DataLoader, val_loader: DataLoader, epochs: int, device: torch.device):
+    output_dir = get_root() / "output"
+    output_dir.mkdir(parents=True, exist_ok=True)
+
     model.to(device)
+    best_accuracy = 0.0
     for i in range(1, epochs+1):
         losses = []
         for utterances, acts in tqdm(train_loader):
@@ -48,4 +58,9 @@ def train_loop(model: torch.nn.Module, loss_fn: torch.nn.Module, optimizer: torc
 
         logging.info(f"Train Loss : {torch.tensor(losses).mean():.3f}")
         model.eval()
-        calculate_loss_accuracy(model, loss_fn, val_loader, device, "Validation")
+        epoch_accuracy = calculate_loss_accuracy(model, loss_fn, val_loader, device, "Validation")
+        if epoch_accuracy > best_accuracy:
+            best_accuracy = epoch_accuracy
+            model_name = f"best_model_epoch_{str(epochs)}_{time.strftime('%Y%m%d')}.pt"
+            torch.save(model.state_dict(), output_dir / model_name)
+            logging.info(f"Best Accuracy : {best_accuracy:.3f} saved as {model_name}.")
